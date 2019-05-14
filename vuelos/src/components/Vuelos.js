3dfdae184dae17f141e944667123432a3f4b25fa
Julian Fajardo <jafajardo37@misena.edu.co>
2019-05-14 18:10:56 -0500

    Chore:Se hace el onChange de idaVuelta

--- vuelos/src/components/Vuelos.js
+++ vuelos/src/components/Vuelos.js
@@ -2,6 +2,12 @@ import React from 'react'
 import Calendario from './Calendario'
 import { FormGroup, Form, Input, Button,Label } from 'reactstrap'
 class Vuelos extends React.Component {
+    state ={
+        idaVuelta: false
+    };
+    handleIdaVuelta=(event)=> {
+        this.setState({idaVuelta: !this.state.idaVuelta});
+    };
     render () {
         return (
             <div>
@@ -16,21 +22,33 @@ class Vuelos extends React.Component {
                             <span className={"text-white col-md-12"}> Ciudad Destino</span><br/>
                             <Input type={"text"} id={"Destino"} placeholder={"Destino"} />
                         </div>
-                        <Calendario id={"Ida"}/>
-                        <Calendario id={"Vuelta"}/>
-                        <div className={"col"}>
-                            <span className={"text-white"}> Pasajeros</span><br/>
-                            <FormGroup>
-                                <Input type="select" name="select" id="exampleSelect">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </Input>
-                            </FormGroup>
-                        </div>
                     </div>
+                    <div className={"row"}>
+                        <div className={"col"}>
+                            <span className={"text-white"}>Tipo de viaje</span><br/>
+                            <Input type="select" name="tipoViaje" id="tipoViaje" onChange={this.handleIdaVuelta}>
+                                <option value={true}>Solo ida</option>
+                                <option value={false}>Ida y vuelta</option>
+                            </Input>
+                        </div>
+                        {this.state.idaVuelta?<> <Calendario id={"Ida"}/> <Calendario id={"Vuelta"}/></> : <Calendario id={"Ida"}/>}
+
+
+                    </div>
+                       <div className={"row"}>
+                           <div className={"col"}>
+                               <span className={"text-white"}> Pasajeros</span><br/>
+                               <FormGroup>
+                                   <Input type="select" name="select" id="exampleSelect">
+                                       <option>1</option>
+                                       <option>2</option>
+                                       <option>3</option>
+                                       <option>4</option>
+                                       <option>5</option>
+                                   </Input>
+                               </FormGroup>
+                           </div>
+                       </div>
                     <div className={"center"}>
                         <Button id={"boton"} >Buscar</Button>
                     </div>
