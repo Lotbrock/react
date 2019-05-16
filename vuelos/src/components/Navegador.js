2d923102bb57a0b8eb51453f028d92608000b8e3
Julian Fajardo <jafajardo37@misena.edu.co>
2019-05-16 07:53:43 -0500

    Chore: se coloca el logo de latam en el navbar

--- vuelos/src/components/Navegador.js
+++ vuelos/src/components/Navegador.js
@@ -1,5 +1,6 @@
 import { stack as Menu } from 'react-burger-menu'
 import { Link } from 'react-router-dom'
+import { NavbarBrand} from 'reactstrap'
 import React from 'react'
 
 class Navegador extends React.Component {
@@ -7,20 +8,28 @@ class Navegador extends React.Component {
     render () {
         return (
             <Menu className="bm-menu">
+                <NavbarBrand><img src={"https://medidasmaletas.com/wp-content/uploads/2017/03/latam-airlines-logo.jpg"} width={"250"} height={"120"}/> </NavbarBrand>
                 <div className="bm-item-list ">
                     <div className="bm-item">
                         <Link className={"colorsitos"} to={"/home"}>Home</Link>
                     </div>
+                </div>
+                <div className="bm-item-list ">
                     <div className="bm-item">
                         <Link className={"colorsitos"} to={"/vuelos"}>Vuelos</Link>
                     </div>
+                </div>
+                <div className="bm-item-list ">
                     <div className="bm-item">
                         <Link className={"colorsitos"} to={"/log-in"}>Log In </Link>
                     </div>
+                </div>
+                <div className="bm-item-list ">
                     <div className="bm-item">
                         <Link className={"colorsitos"} to={"/asientos"}>Asientos </Link>
                     </div>
                 </div>
+
             </Menu>
         );
     }
